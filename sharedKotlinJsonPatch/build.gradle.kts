1062e52b9c56d1a958485edfc772573d2170bf9d
Reid <temphee@gmail.com>
2023-12-04 19:32:45 +0900

    chore: add `kotlinx.serialization.json` dependency.

--- sharedKotlinJsonPatch/build.gradle.kts
+++ sharedKotlinJsonPatch/build.gradle.kts
@@ -26,6 +26,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             //put your multiplatform dependencies here
+            implementation(libs.kotlinx.serialization.json)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
