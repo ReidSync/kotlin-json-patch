322d5bd27c3aea98100e394e694120b4a10d127e
Reid <temphee@gmail.com>
2023-12-06 15:41:00 +0900

    feat: add publishing task and dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,15 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
+    id("io.github.gradle-nexus.publish-plugin").version("1.1.0")
+}
+
+buildscript {
+    dependencies {
+        this.classpath("io.github.gradle-nexus:publish-plugin:1.1.0")
+    }
+}
+
+apply {
+    this.from("scripts/publish-root.gradle")
 }
